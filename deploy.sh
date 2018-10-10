47c1581b07f539a5eef23ac31c74100cd381192a
alex-kuck <alexander.kucksdorf@gmail.com>
2018-10-10 17:54:21 +0200

    Fix login to Dockerhub

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,5 @@ docker build --build-arg VCS_REF=\
  --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`\
  --build-arg DOCKER_TAG="${DOCKER_TAG}"\
  -t ${IMAGE_NAME} .
-echo "$DOCKER_USER"
-echo "$DOCKER_TAG"
-echo "$DOCKER_PWD" | docker login -u "$DOCKER_USER" --password-stdin
+echo '$DOCKER_PWD' | docker login -u "$DOCKER_USER" --password-stdin
 docker push ${IMAGE_NAME}
