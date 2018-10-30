70afb52c1656f8c59bb7be4d6bedaf5572998af6
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-10-30 16:49:30 +0100

    Simplify travis build

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,6 @@
 VCS_REF=`git rev-parse --short HEAD`
 BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 docker-compose build
+if [ "x$TRAVIS_TAG" == "x" ]; then exit; fi
 echo "$DOCKER_PWD" | docker login -u "$DOCKER_USER" --password-stdin
 docker-compose push
-
-#IMAGE_NAME=awesomeinc/jruby-yarn:${DOCKER_TAG}
-#docker build --build-arg VCS_REF=`git rev-parse --short HEAD`\
-# --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`\
-# --build-arg DOCKER_TAG="${DOCKER_TAG}"\
-# -t ${IMAGE_NAME} .
-#echo "$DOCKER_PWD" | docker login -u "$DOCKER_USER" --password-stdin
-#docker push ${IMAGE_NAME}
