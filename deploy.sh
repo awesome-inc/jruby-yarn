548d90b38726d3140fc978952ab2ada0992ab1d8
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-10-30 16:45:40 +0100

    Add jre vs alpine

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-IMAGE_NAME=awesomeinc/jruby-yarn:${DOCKER_TAG}
-docker build --build-arg VCS_REF=`git rev-parse --short HEAD`\
- --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`\
- --build-arg DOCKER_TAG="${DOCKER_TAG}"\
- -t ${IMAGE_NAME} .
+VCS_REF=`git rev-parse --short HEAD`
+BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+docker-compose build
 echo "$DOCKER_PWD" | docker login -u "$DOCKER_USER" --password-stdin
-docker push ${IMAGE_NAME}
+docker-compose push
+
+#IMAGE_NAME=awesomeinc/jruby-yarn:${DOCKER_TAG}
+#docker build --build-arg VCS_REF=`git rev-parse --short HEAD`\
+# --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`\
+# --build-arg DOCKER_TAG="${DOCKER_TAG}"\
+# -t ${IMAGE_NAME} .
+#echo "$DOCKER_PWD" | docker login -u "$DOCKER_USER" --password-stdin
+#docker push ${IMAGE_NAME}
