d2f5de0ad5c981349b644650fd5086925186f63c
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-12-27 11:36:50 +0100

    travis env robustness

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ex
 VCS_REF=`git rev-parse --short HEAD`
 BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+# override DOCKER_TAG with TRAVIS_TAG if set
+export DOCKER_TAG=${TRAVIS_TAG:-9.2.5}
 docker-compose build
 if [ "x$TRAVIS_TAG" == "x" ]; then exit; fi
 echo "$DOCKER_PWD" | docker login -u "$DOCKER_USER" --password-stdin
